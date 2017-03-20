b67e72e52c288047845cadf65497d4df3aba5633
mavlabCourse <info@mavlab.info>
2017-03-20 23:35:12 +0100

    March 20, 2017
    Implemented an initial navigation strategy for Kmeans
    
    Edited files:
    - bepoppy8.xml airframe
    - bepoppy8.xml flightplan
    - bepoppy8.c/bepoppy8.h
    - bepoppy8_cv.cpp/bepoppy8_cv.h
    
    No new files were created.
    
    The code that comes with this commit is of preliminary nature (lots of if statements, etc.)
    Moreover, an additional navigation functions needs to be created that allows the Bebop to
    adjust his heading at an obstruction encounter, without coming to a full stop (as is done
    with set_waypoint_here).
    
    Author: Tijmen Pollack

--- sw/airborne/modules/bepoppy8/bepoppy8_cv.h
+++ sw/airborne/modules/bepoppy8/bepoppy8_cv.h
@@ -28,3 +28,28 @@ extern struct image_t *vision_func(struct image_t *);
 #define logTelemetry(...)
 #endif
 
+struct ArrayInfo{
+	uint8_t InitPoint;;
+	uint8_t ArrayLength;
+};
+
+uint8_t WindowHalfSize;
+
+struct Window{
+	uint8_t LeftBoundary;
+	uint8_t RightBoundary;
+};
+
+struct ClusterInfo{
+	uint8_t Cl1Global;
+	uint8_t Cl2Global;
+	uint8_t Cl3Global;
+	uint8_t Cl1AvoidLeft;
+	uint8_t Cl2AvoidLeft;
+	uint8_t Cl3AvoidLeft;
+	uint8_t Cl1AvoidRight;
+	uint8_t Cl2AvoidRight;
+	uint8_t Cl3AvoidRight;
+};
+
+struct ClusterInfo Environment;
