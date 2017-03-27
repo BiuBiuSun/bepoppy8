b457bb391ef7ea9580d75bd87e28b0aae7f8aaaa
Dave Kroezen <davekroezen@gmail.com>
2017-03-27 17:59:27 +0200

    Fixed uint8_t > uint32_t oepsie!

--- sw/airborne/modules/bepoppy8/bepoppy8_cv.cpp
+++ sw/airborne/modules/bepoppy8/bepoppy8_cv.cpp
@@ -6,7 +6,7 @@
 #define BOARD_CONFIG "boards/bebop.h"
 #include "bepoppy8_cv.h"
 #include "modules/computer_vision/opencv_image_functions.h"
-
+#include <iostream>
 using namespace std;
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -16,7 +16,7 @@ using namespace cv;
 // Define local functions:
 Mat uv_channels(struct image_t *);
 Mat cluster_image(struct image_t *);
-uint8_t SearchFloor(Mat);
+uint8_t SearchFloor(Mat, struct image_t *);
 void write_clusterLabels(Mat);
 Mat load_clusterLabels();
 Mat yuv422_to_ab(struct image_t *img);
@@ -36,21 +36,21 @@ double eps 			= 0.1;
  */
 struct image_t *vision_func(struct image_t *img) {
 
-	printf("[vision_func()] Started\n");
+//	printf("[vision_func()] Started\n");
 
 	Mat clusterLabels = cluster_image(img);
 
-	printf("BestLabels retrieved\n");
+//	printf("BestLabels retrieved\n");
 
-	uint8_t FloorID = SearchFloor(clusterLabels);
+	uint8_t FloorID = SearchFloor(clusterLabels, img);
 
-	printf("I found the floor\n");
+	printf("I found the floor: %d\n", FloorID);
 
 	NavWindow = occlusionDetector(clusterLabels, FloorID);
 
 	printf("Window %d seems the best option\n", *NavWindow);
 
-	printf("[vision_func()] Finished\n");
+//	printf("[vision_func()] Finished\n");
 
   return img;
 }
@@ -64,7 +64,7 @@ struct image_t *vision_func(struct image_t *img) {
  * 			:  					- Image buffer is set to the segmented image.
  */
 Mat cluster_image(struct image_t *img) {
-		printf("[cluster_image()] Start\n");
+//		printf("[cluster_image()] Start\n");
 		uint8_t *img_buf 	= (uint8_t *) img->buf;			// Get image buffer
 
 		// K-means allocation
@@ -82,7 +82,7 @@ Mat cluster_image(struct image_t *img) {
 		// Check if clusters are initialized:
 		if (init_cluster.empty()){
 			kmeans(data, clusters, init_cluster, TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, attempts, eps), attempts, KMEANS_PP_CENTERS, centers);
-			printf("EMPTY\n");
+//			printf("EMPTY\n");
 		}
 		kmeans(data, clusters, init_cluster, TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, attempts, eps), attempts, KMEANS_USE_INITIAL_LABELS, centers);
 		clusterLabels = init_cluster;
@@ -92,7 +92,7 @@ Mat cluster_image(struct image_t *img) {
 			Mat img_intermediate(2*clusterLabels.rows,1,CV_32SC1), img_intermediate2, img_seg;
 			float scale 		= 255.0/clusters;
 
-			printf("clusterLabels: Rows: %d Cols: %d Channels: %d Type: %d\n" , clusterLabels.rows, clusterLabels.cols, clusterLabels.channels(), clusterLabels.type());
+//			printf("clusterLabels: Rows: %d Cols: %d Channels: %d Type: %d\n" , clusterLabels.rows, clusterLabels.cols, clusterLabels.channels(), clusterLabels.type());
 
 			int index = 0;
 			// Restore size of UV:
@@ -101,16 +101,16 @@ Mat cluster_image(struct image_t *img) {
 				img_intermediate.at<float>(2*i + 1) = clusterLabels.at<float>(i);
 				index++;
 			}
-			printf("Length of restore loop: %d\n", index);
+//			printf("Length of restore loop: %d\n", index);
 
 			// Process from clusterLabels
 			img_intermediate.convertTo(img_intermediate2, CV_8UC1, scale, 0);
-			applyColorMap(img_intermediate2, img_seg, COLORMAP_JET);
+			applyColorMap(img_intermediate2, img_seg, COLORMAP_BONE);
 			cvtColor(img_seg, img_seg, COLOR_BGR2YUV);
 
 			yuv_to_yuv422(img_seg, (char *) img_buf);
 		}
-		printf("[cluster_image()] Finished\n");
+//		printf("[cluster_image()] Finished\n");
 	return clusterLabels;
 }
 
@@ -118,13 +118,13 @@ Mat cluster_image(struct image_t *img) {
  * Local function to allocate the UV channels to a format k-means is able to process.
  */
 Mat uv_channels(struct image_t *img) { // TODO: Investigate possible performance gain using: .split(uv Y) transpose(uv) reshape(1,2) transpose(uv)
-	printf("[uv_channels()] Started\n");
+//	printf("[uv_channels()] Started\n");
 	uint8_t *img_buf = (uint8_t *) img->buf;
-	printf("[uv_channels()] 1\n");
+//	printf("[uv_channels()] 1\n");
 	Mat uv(img->h*img->w*0.5,2,CV_32FC1); // reshape to 2 rows
-	printf("[uv_channels()]2\n");
+//	printf("[uCv_channels()]2\n");
 
-	printf("%d\n",img->h);
+//	printf("%d\n",img->h);
 
 	for (uint16_t y = 0; y < img->h; y++) {
 	    for (uint16_t x = 0; x < img->w; x += 2) {
@@ -132,10 +132,9 @@ Mat uv_channels(struct image_t *img) { // TODO: Investigate possible performance
 	    	uv.at<float>((y*img->w + x)/2, 1) = img_buf[2]; // V
 	    	img_buf += 4;
 	    }
-
 	}
 
-	printf("[uv_channels()] Done\n");
+//	printf("[uv_channels()] Done\n");
 	return uv;
 }
 
@@ -149,27 +148,41 @@ Mat uv_channels(struct image_t *img) { // TODO: Investigate possible performance
  * Output	: uint8_t FloorCluster 	- The cluster label (unsigned char) corresponding to the floor
  */
 
-uint8_t SearchFloor(Mat clusterLabels){
+uint8_t SearchFloor(Mat clusterLabels, struct image_t *img){
 
 	struct ClusterInfo Environment = {0,0,0};
 	int rowScans = 5;
 
-	for(int r = clusterLabels.rows - rowScans; r < clusterLabels.rows; r++)
-	{
-		for(int c = 0; c < clusterLabels.cols; c++){
-			if(clusterLabels.at<uchar>(r,c) == 0){
+	int errors = 0;
+
+	for(int c = 0; c < img->h; c++) {
+		int start = c * img->w/2;
+
+		for(int r = start; r < start + rowScans; r++) {
+
+			switch (clusterLabels.at<uint8_t>(r)) {
+			case 0:
 				Environment.Cl0Global++;
-			} else if(clusterLabels.at<uchar>(r,c) == 1){
+				break;
+			case 1:
 				Environment.Cl1Global++;
-			} else Environment.Cl2Global++;
+				break;
+			case 2:
+				Environment.Cl2Global++;
+				break;
+			default:
+				printf("[SearchFloor] Error caused by value of: %d\n", clusterLabels.at<uint8_t>(r));
+				errors++;
+				break;
+			}
 		}
 	}
 
 	uint8_t FloorCluster = 0;
-	if(Environment.Cl1Global>=Environment.Cl0Global){
+	if(Environment.Cl1Global>=Environment.Cl0Global && Environment.Cl1Global>=Environment.Cl2Global){
 			FloorCluster = 1;
 		}
-		else if(Environment.Cl2Global>=Environment.Cl1Global){
+		else if(Environment.Cl2Global>=Environment.Cl1Global && Environment.Cl2Global>=Environment.Cl0Global){
 			FloorCluster = 2;
 		}
 
@@ -224,19 +237,19 @@ Mat yuv422_to_ab(struct image_t *img) {
 			img_buf += 4;
 		}
 	}
-	printf("[yuv2ab()] YUV put in matrix\n");
+	//printf("[yuv2ab()] YUV put in matrix\n");
 	//printf("[yuv2ab()] YUV about to be reshaped.\nRows: %d, Cols: %d, Channels: %d\n", yuv.rows, yuv.cols, yuv.channels());
 	//yuv.reshape(3, yuv.rows);
-	printf("[yuv2ab()] YUV reshaped.\nRows: %d, Cols: %d, Channels: %d\n", yuv.rows, yuv.cols, yuv.channels());
+	//printf("[yuv2ab()] YUV reshaped.\nRows: %d, Cols: %d, Channels: %d\n", yuv.rows, yuv.cols, yuv.channels());
 	cvtColor(yuv, BGR, CV_YUV2BGR);
-	printf("[yuv2ab()] YUV2BGR\n");
+	//printf("[yuv2ab()] YUV2BGR\n");
 	cvtColor(BGR, Lab, CV_BGR2Lab);
-	printf("[yuv2ab()] BRG2Lab\n");
+	//printf("[yuv2ab()] BRG2Lab\n");
 
 	split(Lab, Lab2);
-	printf("[yuv2ab()] Lab split\n");
+	//printf("[yuv2ab()] Lab split\n");
 	hconcat(Lab2[0],Lab2[1],out);
-	printf("[yuv2ab()] ab converted - done\n");
+	//printf("[yuv2ab()] ab converted - done\n");
 	out.convertTo(out,CV_32FC1);
 
 	return out;
@@ -274,6 +287,8 @@ int * occlusionDetector(Mat seg_image, uint8_t ground_id)
 	int window_size = seg_image.cols/NUM_WINDOWS; // width of a single window
 	int window_avg; // holds window average in loop
 
+//	cout << seg_image.rows << endl;
+//	cout << seg_image.cols << endl;
 	for(int window = 0; window < NUM_WINDOWS; window ++)
 	{
 		window_avg = 0; // Reset window average
@@ -304,7 +319,7 @@ int * occlusionDetector(Mat seg_image, uint8_t ground_id)
 			*bestWindow = window-NUM_WINDOWS/2;
 		}
 
-		//cout << "Window " << window-NUM_WINDOWS/2 << " average:\t" << window_avg << endl; // print window average
+//		cout << "Window " << window-NUM_WINDOWS/2 << " average:\t" << window_avg << endl; // print window average
 
 	}
 
